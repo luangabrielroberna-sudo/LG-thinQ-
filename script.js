6c5f4a83db91cd4dad86218649c9e71df7f0f1e8
Luanyou <luangabrielroberna@gmail.com>
2026-01-10 21:42:54 -0300

    Add toggle function for button status

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function toggle(btn) {
+  const status = btn.previousElementSibling.querySelector("span");
+
+  if (status.classList.contains("on")) {
+    status.classList.remove("on");
+    status.classList.add("off");
+    status.innerText = "Desligado";
+    btn.innerText = "Ligar";
+  } else {
+    status.classList.remove("off");
+    status.classList.add("on");
+    status.innerText = "Ligado";
+    btn.innerText = "Desligar";
+  }
+}
 
